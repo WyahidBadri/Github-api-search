9e36cbf597d3bdbc6c001d53e031ac466c51edba
Wyahid Badri <wyahidb2015@gmail.com>
2021-11-02 17:01:13 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import Repodetail from './repoDetail';
 import Searchrepo from './searchRepo';
 import Sortedrepo from './sortedRepo'
-import './App.css';
 
 
 
